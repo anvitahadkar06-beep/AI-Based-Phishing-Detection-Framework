93f614562c90ada5230ca739d096f217d9e3b2cc
pranjali0621 <mahadikpranjali37@gmail.com>
2026-07-13 20:43:47 +0530

    Frontend ready for deployment

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://ai-based-phishing-detection-framework.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 30000,
 });
+
+export const scanURL = async (url) => {
+  const response = await API.post("/predict", {
+    url: url,
+  });
+
+  return response.data;
+};
 
 export default API;
