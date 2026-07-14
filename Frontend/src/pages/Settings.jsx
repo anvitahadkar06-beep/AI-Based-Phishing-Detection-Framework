0aee5b182d900a5e4510dfa4af701d1546b5a9aa
Anvita <anvita.hadkar06@gmail.com>
2026-07-14 17:51:07 +0530

    Update Settings.jsx

--- Frontend/src/pages/Settings.jsx
+++ Frontend/src/pages/Settings.jsx
@@ -1,1 +1,10 @@
-//
+function Settings() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Settings</h1>
+      <p>This page is under development.</p>
+    </div>
+  );
+}
+
+export default Settings;
